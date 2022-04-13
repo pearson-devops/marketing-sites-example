4da5a34e875c3abe526ded1a7e4ff90e85ae93b5
Mike Schlosser <ms122r@gmail.com>
2022-04-13 09:48:24 -0500

    dynamic admin page

--- apps/und/pages/admin.js
+++ apps/und/pages/admin.js
@@ -1,2 +1,11 @@
-import { TinaAdmin } from "tinacms";
+import dynamic from 'next/dynamic';
+const TinaAdmin = dynamic(() => import('tinacms').then((module) => module.TinaAdmin));
+
+export function getStaticProps() {
+    return {
+      // returns the default 404 page with a status code of 404
+      notFound: process.env.NODE_ENV === 'production'
+    }
+}
+
 export default TinaAdmin;
