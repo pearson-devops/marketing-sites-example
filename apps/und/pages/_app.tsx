7b36c832bfa55c6263c47e70629446bc7e3b93ec
Mike Schlosser <ms122r@gmail.com>
2022-04-13 09:25:16 -0500

    Tina Dynamic Import

--- apps/und/pages/_app.tsx
+++ apps/und/pages/_app.tsx
@@ -3,11 +3,18 @@ import { Layout } from "../components/layout";
 import TinaProvider from "../.tina/components/TinaDynamicProvider";
 
 const App = ({ Component, pageProps }) => {
-  return (
-    <TinaProvider>
+
+  if(process.env.NODE_ENV === "development") {
+    return (
+      <TinaProvider>
+        <Component {...pageProps} />
+      </TinaProvider>
+    )
+  } else {
+    return (
       <Component {...pageProps} />
-    </TinaProvider>
-  );
+    )
+  }
 };
 
 export default App;
